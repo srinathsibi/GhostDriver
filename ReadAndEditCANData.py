005010e24c114de45436d9ba038ab455fe9e2f0c
srinathsibi <sibisrinath@gmail.com>
2018-08-08 01:44:20 -0700

    Finished the CAN and IMU relevant data extraction

--- ReadAndEditCANData.py
+++ ReadAndEditCANData.py
@@ -17,8 +17,8 @@ except NameError:
 
 ## Opening a new file to write data that is being read from the main CAN file
 filename =  raw_input(' Please Enter the file name for the clipped CAN file ')
-time_start = raw_input(' Please enter the start time for clipping ')
-time_stop = raw_input(' Please enter the stop time for clipping ')
+time_start = raw_input(' Please enter the start time for clipping CAN data')
+time_stop = raw_input(' Please enter the stop time for clipping CAN data')
 # Outputting the parameters to confirm the input
 print " Start time:",type(time_start)," ",time_start,"\n", "Stop Time: ",type(time_stop)," ",time_stop,"\n",	\
 	" File Name: " , type(filename)," ",filename
@@ -39,16 +39,28 @@ out_file.close()
 
 
 ############################################################################################################
-################# Reading the IMU file #####################################################################
+################# Reading the IMU file ############################################################################################################
 try:
-	os.chdir("../GDData/IMU")	#Path for CAN data
-	f = open('imuLog_1532641633.csv')	#Name of CAN data file
+	os.chdir("../../GDData/IMU")	#Path for CAN data
+	f = open('imuLog_1532371925.csv')	#Name of CAN data file
 	csv_f = csv.reader(f)
 except NameError:
 	print "No such file exists!!!!"
 ## Opening a new file to write data that is being read from the main CAN file
 filename =  raw_input(' Please Enter the file name for the clipped IMU file ')
-
+time_start = raw_input(' Please enter the start time for clipping IMU data')
+time_stop = raw_input(' Please enter the stop time for clipping IMU data')
 out_file = open(filename, 'w')
+with out_file:
+	for row in csv_f:
+		#print(row)		
+		array = [i.split(',',2)[0] for i in row]
+		if float(array[0]) >= float(time_start) and float(array[0]) <= float(time_stop):
+			#print type(float(array[0]))," : ",float(array[0])		
+			writer = csv.writer(out_file)
+			writer.writerows([row])
+## Closing files after reading and writing
+f.close()
+out_file.close()
 
 

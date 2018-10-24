28823c903eba1cd3bff56ed9be8e3e67dfdb8ce8
srinathsibi <sibisrinath@gmail.com>
2018-10-24 14:13:11 -0400

    Iterative Video Combinatino

--- CombineVideos.py
+++ CombineVideos.py
@@ -4,7 +4,10 @@
 # GoPro files are broken down into  4 GB files as we keep recording. The numbers in a GOPRO file name indicate the order of...
 # file creation. For example for files ['GP019354.MP4', 'GOPR9354.MP4', 'GP029354.MP4']...
 # The first file ends with numbers 9354, then 019354 and 029354 and so on. So we combine the files starting with lowest..
-# number in filename and then move to the maximum number in the filenames. For now , a simple sort() functio
+# number in filename and then move to the maximum number in the filenames. For now , a simple sort() function
+
+# Prereqs : This piece of code uses MoviePy library. To be able to use this, you need to install pip (python package manager),
+#and then use pip to install moviepy . The instructions: http://zulko.github.io/moviepy/install.html
 import csv,argparse,sys,os
 import subprocess, re
 from decimal import Decimal
@@ -16,7 +19,7 @@ try:
     filelist = glob.glob('*.MP4') # Searching for all files with MP4 extension
     print "List of MP4 files in the folder right now" , filelist, "  ", type(filelist[1])
 except NameError:
-    print "Error Encountered" # Make sure you are in the right folder if this error messsage pops output
+    print "Error Encountered" # Make sure you are in the right folder if this error messsage pops up.
     # It could also be lack of any MP4 files in the folder
 if len(filelist) > 1:
     print "here we go!"
@@ -26,8 +29,19 @@ else:
     print "Insufficent number of files in folder"
 #ffmpeg -i "concat:GOPR9354.MP4|GP019354.MP4|GP029354.MP4" -c:a copy -c:v copy output.mp4
 #ffmpeg is cauding weird errors. Output file is smaller than it should be and the above command needs to be run from bash => Not optimal
-clip1 = VideoFileClip(filelist[0])
-clip2 = VideoFileClip(filelist[1]).subclip(50,60)
-clip3 = VideoFileClip(filelist[2])
-final_clip = concatenate_videoclips([clip1,clip2,clip3])
+
+#Non - Iterative Clip Formation
+#clip1 = VideoFileClip(filelist[0]).subclip(0,5)
+#clip2 = VideoFileClip(filelist[1]).subclip(0,5)
+#clip3 = VideoFileClip(filelist[2]).subclip(0,5)
+#cliplist = [clip1,clip2,clip3]
+
+#Iterative clip formation
+cliplist = [ VideoFileClip(name) for name in filelist ]
+#Concatenation procedure
+final_clip = concatenate_videoclips([clip for clip in cliplist])
 final_clip.write_videofile("my_concatenation.mp4")
+
+# Unfortunately, this piece of code cannot be iterated, so I am hoping that there are no more than 5 MP4 files in individual folders...
+for i in range(len(filelist)):
+    cliplist[i] = VideoFileClip(filelist[i])

e030f23d921a2e5d131f8388d4ac3803f1653816
srinathsibi <sibisrinath@gmail.com>
2018-10-26 04:06:00 -0400

    Starting CAN data parser

---
+++ CANDataConverter.py
@@ -0,0 +1,4 @@
+# Author: Srinath Sibi
+#Users: Becky Currano and Dylan Moore
+#Purpose: Parse the CAN data to convert codes to actual values.
+#Note: This code is based on Nik's convertCAN.py code which is also in the github repo
